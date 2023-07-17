eb068466a99f5622244de8a34cbad196fcd30190
srx <SeraphUltimateX@gmail.com>
2023-07-17 23:52:44 +0700

    Add extensions for Course and Lesson

---
+++ YogaCenterWinApp_Group9/DisplayModels/CourseExtensions.cs
@@ -0,0 +1,41 @@
+﻿using YogaCenter.Repository.Models;
+using static YogaCenterWinApp_Group9.DisplayModels.CourseStatus;
+
+namespace YogaCenterWinApp_Group9.DisplayModels;
+
+public static class CourseExtensions
+{
+    public static string GetCourseCode(this Course course)
+    {
+        var program = course.Program
+            ?? throw new ArgumentException("Course is not linked to a Program.", nameof(course));
+        return program.Code
+            + course.CourseNumber.ToString();
+    }
+
+    public static string GetStatusName(this Course course)
+    {
+        try
+        {
+            if (course.StartDate == null || course.EndDate == null)
+            {
+                return StatusNames[0];
+            }
+            if (course.RegistrationOpenDate == null || course.RegistrationCloseDate == null)
+            {
+                return StatusNames[1];
+            }
+
+            DateTime now = DateTime.Now;
+            if (now < course.RegistrationOpenDate) { return StatusNames[2]; }
+            else if (now <= course.RegistrationCloseDate) { return StatusNames[3]; }
+            else if (now < course.StartDate) { return StatusNames[4]; }
+            else if (now <= course.EndDate) { return StatusNames[5]; }
+            else { return StatusNames[6]; }
+        }
+        catch (Exception)
+        {
+            return StatusNames[-1];
+        }
+    }
+}
---
+++ YogaCenterWinApp_Group9/DisplayModels/CourseStatusNames.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using YogaCenter.Repository.Models;
+
+namespace YogaCenterWinApp_Group9.DisplayModels;
+
+public static class CourseStatus
+{
+    public static CourseStatusNames StatusNames { get; } = new();
+
+    public class CourseStatusNames : IEnumerable<StatusIdNamePair>
+    {
+        private static Dictionary<int, string> StatusNames { get; set; } = new Dictionary<int, string>()
+        {
+            { -1, "<COULD NOT BE DETERMINED>" },
+            { 0, "Schedule Not Set" },
+            { 1, "Registration Dates Pending" },
+            { 2, "Registration Not Open Yet" },
+            { 3, "Registration Open" },
+            { 4, "Registration Closed; Ready to Start" },
+            { 5, "Course In Progress" },
+            { 6, "Course Finished" },
+        };
+
+        public string this[int index]
+        {
+            get => StatusNames[index];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerator<StatusIdNamePair> GetEnumerator()
+        {
+            foreach (var statusName in StatusNames)
+            {
+                yield return new StatusIdNamePair
+                {
+                    StatusId = statusName.Key,
+                    StatusName = statusName.Value
+                };
+            }
+        }
+    }
+
+    public struct StatusIdNamePair
+    {
+        public int StatusId { get; set; }
+        public string StatusName { get; set; }
+    }
+}
